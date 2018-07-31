858d202ffb31c9774d41ecd860f084f4fa0c0992
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2018-07-31 22:55:22 +0530

    Version 1: Host previous website

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(function() {
+  let typed = new Typed('#about-me', {
+    strings: [
+      'Student at Delhi University.',
+      'Tech. Core Contributor at Mozilla Delhi.', 'Open Source Enthusiast.'
+    ],
+	typeSpeed: 20,
+	backSpeed: 20,
+    loop: true,
+  });
+});

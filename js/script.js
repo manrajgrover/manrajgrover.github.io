d68c61881efa2406222ac96fce718158814a549f
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2018-12-02 02:52:53 +0530

    [Revamp] Adds question answer section and footer

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
-$(function() {
-  let typed = new Typed('#about-me', {
-    strings: [
-      'Software Engineer at Practo.',
-      'Budding Researcher.',
-      'Open Source Contributor.'
-    ],
-	typeSpeed: 20,
-	backSpeed: 20,
-    loop: true,
+$(document).ready(function() {
+  $('.question').on('click', function() {
+    var qaContainer = $(this).parent();
+    qaContainer.css({margin: '0.5em 0em'});
+
+    var answer = qaContainer.find('.answer');
+    var height = $(this).height() + 'px';
+
+    answer.css({
+      minHeight: height
+    });
+
+    $(this).hide();
+    answer.show().addClass("expand-0-5");
   });
 });
